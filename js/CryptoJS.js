81232dbdc101c68288d8e29f3fe85eb6b3d9f2e9
vtdeleond <vtdeleond@gmail.com>
2016-05-07 13:27:29 -0400

    Get hash in javascript

---
+++ js/CryptoJS.js
@@ -0,0 +1,16 @@
+(function() {
+    ///
+    //https://code.google.com/p/crypto-js/
+    function getMD5() {
+        //var tmpvt = $(document).find('form').html();//document.documentElement.innerHTML;
+        var tmpvt = new Array();
+        var tpHash;
+        //$(document).find('input').each(function(){
+        $(document).find('input:not(#idPROXIMO, #__isProcessing, #idXSEEDMSG)').each(function(){
+            tmpvt.push($(this).val());
+        });
+    tmpvt.sort();
+    tpHash = CryptoJS.MD5(tmpvt.join());
+    return tpHash;
+  }  
+});
