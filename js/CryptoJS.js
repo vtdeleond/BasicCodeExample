118318589df026a1d69cfbdc0d15568c2082f29d
vtdeleond <vtdeleond@gmail.com>
2016-05-07 13:58:16 -0400

    New source
    New source

--- js/CryptoJS.js
+++ js/CryptoJS.js
@@ -9,8 +9,32 @@
         $(document).find('input:not(#idPROXIMO, #__isProcessing, #idXSEEDMSG)').each(function(){
             tmpvt.push($(this).val());
         });
-    tmpvt.sort();
-    tpHash = CryptoJS.MD5(tmpvt.join());
-    return tpHash;
-  }  
+        tmpvt.sort();
+        tpHash = CryptoJS.MD5(tmpvt.join());
+        return tpHash;
+    }
+
+    function tpAsignaEventos() {
+        $('#prueba').on('click', function() {
+            getMD5();
+        });
+        //
+        $('#idBUTTON1').on('click', function() {
+            tpAsignaEventosOnclickButton();
+        }).prop({'onclick' : '', 'onkeydown' : ''});
+        //
+        function tpAsignaEventosOnclickButton () {
+            var tpAccion = $('#idACCION').val();
+            if (tpAccion == 'CON') {
+                var tpHashCON = getMD5();
+                if (tpHashCON.words.join() != tmpvtd2.words.join()) {
+                    if (!confirm('Hubo cambios en la trasacción, perdera los cambios. ¿Proceder? ')) {
+                        return false;
+                    }
+                }
+            }
+            jXmit();
+            flagEnter = 0;
+        }
+    }
 });
---
+++ js/getUrlVars.js
@@ -0,0 +1,9 @@
+(function() {
+    function getUrlVars() {
+        var vars = {};
+        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+            vars[key] = value;
+        });
+        return vars;
+    }
+});
